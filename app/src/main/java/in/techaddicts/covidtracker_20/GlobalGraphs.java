44a607e9d4d40f4726138558e257fb84d9351f94
Jaskirat Singh <jaskirats25d@gmail.com>
2020-08-02 11:56:51 +0530

    Initial Commit

---
+++ app/src/main/java/in/techaddicts/covidtracker_20/GlobalGraphs.java
@@ -0,0 +1,37 @@
+package in.techaddicts.covidtracker_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class GlobalGraphs extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_graphs);
+
+        webView = (WebView) findViewById(R.id.id_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://covid19.who.int/?gclid=Cj0KCQjwpZT5BRCdARIsAGEX0zkFKder5Q7nj3kDZk-PZsiAUT3n-C4QVGSq4A2vhPpZ9GVZ8Chjz6gaArKPEALw_wcB");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/in/techaddicts/covidtracker_20/TrackIndia.java
@@ -0,0 +1,36 @@
+package in.techaddicts.covidtracker_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class TrackIndia extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_india);
+
+        webView = (WebView) findViewById(R.id.id_webview2);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.worldometers.info/coronavirus/country/india/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
